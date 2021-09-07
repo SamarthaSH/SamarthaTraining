7ec23b50e702b43a4c2c1740592d2699f1d7ea34
SamarthaSH <90250352+SamarthaSH@users.noreply.github.com>
2021-09-07 22:11:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace SampleConApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm Samartha");
+            Console.WriteLine("I'm from Shimoga");
+            Console.WriteLine("I'm a Quality Engineer");
+
+        }
+    }
+}
